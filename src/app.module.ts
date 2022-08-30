2ab91d96cf1e465fee180ebe2094c74944c0f105
bo yang <1070939301@qq.com>
2022-08-30 20:21:43 +0800

    feat:新增订单接口；订单表列表接口；订单详情接口；

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { FoodsModule } from './foods/foods.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/ScanOrder'),
     FoodsModule,
+    OrdersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
